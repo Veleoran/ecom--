52159cdae2f441e284cd1d5b67dd3408b70dbfe4
JulienPoirierWebDev <connexion@julienpoirier-Webdev.com>
2024-01-08 10:40:08 +0100

    colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      "dark-primary": "#2A254B",
+      "primary": "#4E4D93",
+      "light-grey": "#F9F9F9",
+      "border-grey": "#EBE8F4",
+      "white": "#FFFFFF",
+      "border-dark": "#CAC6DA",
+    },
   },
   plugins: [],
 };
